9c04224791bedb2d8f47507116d33d8643825c33
Paul Water <p.r.water@setbased.nl>
2018-07-13 10:50:40 +0200

    Initial commit.

---
+++ src/Dispatcher.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SetBased\Abc\Event;
+
+/**
+ * The event dispatcher.
+ */
+interface Dispatcher
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Dispatches all queued events (as MAY be queued by notify()).
+   */
+  public function dispatch(): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Provide all listeners with an event to modify.
+   *
+   * @param Event $event The event to modify.
+   *
+   * @return Event The event that was passed, now modified by callers.
+   */
+  public function modify(Event $event): Event;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Notify listeners of an event.
+   *
+   * This method MAY act asynchronously. Callers SHOULD NOT assume that any action has been taken when this method
+   * returns.
+   *
+   * @param Event $event The event to notify listeners of.
+   *
+   * @return void
+   */
+  public function notify(Event $event): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
